93ed4d7927d7766df94dae4b2cb70fb516b93fb2
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-06-14 15:52:15 +0500

    javaScript Project

---
+++ app.js
@@ -0,0 +1,334 @@
+// CHAPTER #01
+
+// TASK 01
+// alert("Hello World")
+
+// Task 02
+// alert("Error! Please Enter a valid password")
+
+// Task 03
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// Task 04
+// alert("Welcome to JS Land")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs")
+
+// Task 05 in browser console
+
+
+// CHAPTER #02
+
+// Task 01
+// var userName = "Rameez"
+
+// Task 02
+// var myName = "Rameez Ahmed Qazi"
+
+// Task 03
+// var message = "Hello World!"
+// alert(message)
+
+// Task 04
+// var name = "Rameez Ahmed Qazi"
+// alert(name)
+// var age = "20 Years Old"
+// alert(age)
+// var currentlyEnrolled = "Student at Certified Web & Mobile App development Course"
+// alert(currentlyEnrolled)
+
+// Task 05
+// var food = "PIZZA"
+// var food2 = "PIZZ"
+// var food3 = "PIZ"
+// var food4 = "PI"
+// var food5 = "P"
+// alert(food +"\n"+food2+"\n"+food3+"\n"+food4+"\n"+food5)
+
+// Task 06
+// var email = "rameez.qazi23@gmail.com"
+// alert("My email address is "+email)
+
+// Task 07
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+
+// Task 08
+// var message = "Yah! I can write HTML content through JavaScript"
+// document.write(message)
+
+// Task 09
+// var specialChar = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(specialChar)
+
+// CHAPTER #03
+
+// Task 01
+// var age = 20
+// alert("I am "+age+" years Old")
+
+// Task 02
+// var count = 1
+// var count = count++
+// alert("You have visited"+ count +" times")
+
+// Task 03
+// var birthYear = 1998
+// document.write("My Birth year is"+ birthYear +"<br>")
+// document.write("Data type of my declared variable is number")
+
+// Task 04
+// var visitorsName = "John Doe".bold()
+// var productTitle = "T-Shirt".bold()
+// var quantity = "5".bold()
+// document.write(visitorsName +" ordered "+ quantity + productTitle +"(s) on XYZ Clothing store")
+
+
+// CHAPTER 04
+
+// TASK 01
+// var name, age, gender;
+
+// TASK 02
+// Legal Variable Names
+// var name
+// var _age
+// var myName2
+// var my_name
+// var my_2nd_age
+
+// Illegal Variable Names
+// var 2name
+// var !name
+// var my-first-name
+// var gender!
+// var @name
+
+// TASK 03
+// var a = "A heading stating “Rules for naming JS variables”".bold()
+// document.write(a+"<br>" + "<br>")
+// var b1,b2,b3,b4,b5,c1,c2,c3,c4,d,e;
+// b1 = ","
+// b2 = "number"
+// b3 = "$"
+// b4 = "_"
+// b5 = "$my_1stVariable".bold()
+
+// c1 = "letter"
+// c2 = "$"
+// c3 = "_"
+// c4 = "$name, _name or name".bold()
+
+// d = "case sensitive"
+
+// e = "keywords"
+
+
+// document.write("Variable names can only contain \t"+ b1 +" "+ b2 +"\t,\t"+ b4 +"\t . For example \t"+ b5 )
+// document.write("<br>")
+// document.write("Variable must begin with a \t"+ c1 +" "+ c2 +"\t,\t"+ c3 +"\t . For example \t"+ c4)
+// document.write("<br>")
+// document.write("Variable names are "+ d)
+// document.write("<br>")
+// document.write("Variable name should not be JS "+ e)
+
+
+// CHAPTER #05
+
+// TASK 01
+// var num1 = 12;
+// var num2 = 18;
+// var sum = num1+num2
+// document.write("Sum of \t"+ num1 +"\t and \t"+ num2 +"\t is \t" + sum +"<b)
+
+// TASK 02
+// var num1 = 12;
+// var num2 = 18;
+// var sub = num1-num2
+// document.write("Subtraction of \t"+ num1 +"\t and \t"+ num2 +"\t is \t" + sub +"<br>")
+
+// var num1 = 12;
+// var num2 = 18;
+// var mul = num1*num2
+// document.write("Multiplication of \t"+ num1 +"\t and \t"+ num2 +"\t is \t" + mul +"<br>")
+
+// var num1 = 12;
+// var num2 = 18;
+// var div = num1/num2
+// document.write("Division of \t"+ num1 +"\t and \t"+ num2 +"\t is \t" + div +"<br>")
+
+// var num1 = 12;
+// var num2 = 18;
+// var mod = num1%num2
+// document.write("Modulus of \t"+ num1 +"\t and \t"+ num2 +"\t is \t" + mod)
+
+// TASK 03
+// var firstVar = 
+// document.write("Value after variable declaration is: \t"+ firstVar +"<br>")
+// var firstVar = 5
+// document.write("Initial value is: \t"+ firstVar +"<br>")
+
+// var firstVar = ++firstVar
+// document.write("Value after increment is: \t"+ firstVar +"<br>")
+
+// var firstVar = firstVar + 7
+// document.write("Value after addition is: \t"+ firstVar +"<br>")
+
+// var firstVar = --firstVar
+// document.write("Value after decrement is: \t"+ firstVar +"<br>")
+
+// var firstVar = firstVar%3
+// document.write("The remainder is: \t"+ firstVar +"<br>")
+
+// TASK 04
+// var ticketPrice = 600
+// var totalTickets = 5
+// var ticketPrice = ticketPrice*totalTickets
+// document.write("Total cost to buy \t"+ totalTickets +"\t tickets to a movie is \t"+ ticketPrice +"PKR")
+
+// TASK 05
+// document.write("Table of 4".bold() +"<br>")
+// var num = 4
+// var num2 = 1
+// document.write("4 * "+ num2 +" = " + num +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write("4 * "+ ++num2 +" = " + num*num2 +"<br>")
+
+// TASK 06
+// var cTemp = 30
+// var CintoF = ((cTemp * 9/5) + 32)
+// document.write("Temperature "+ cTemp +"°C into Fahrenheit is " + CintoF +"°F<br>")
+// var FintoC = ((CintoF - 32) * 5/9)
+// document.write("Temperature "+ CintoF +"°F into Celcius is " + FintoC +"°C<br>")
+
+// TASK 07
+// var priceItem1 = 650
+// var priceItem2 = 100
+// var quantityItem1 = 3
+// var quantityItem2 = 7
+// var shippingCharges = 100
+// var item1Price = priceItem1*quantityItem1
+// var item2Price = priceItem2*quantityItem2
+
+// document.write("Shopping Cart".bold() +"<br>"+"<br>") 
+// document.write("Price of item 1 is "+ priceItem1 +"<br>")
+// document.write("Price of item 2 is "+ priceItem2 +"<br>")
+// document.write("Quantity of item 1 is "+ quantityItem1 +"<br>")
+// document.write("Quantity of item 2 is "+ quantityItem2 +"<br>")
+// document.write("Total cost of your order is "+ (item1Price+item2Price+shippingCharges)) 
+
+// TASK 08
+// var totalMarks = 980
+// var marksObtained = 804
+// var percentage = ((marksObtained/totalMarks) * 100)
+
+// document.write("Mark Sheet".bold() +"<br>"+"<br>")
+// document.write("Total Marks: "+ totalMarks +"<br>")
+// document.write("Marks Obtained: "+ marksObtained +"<br>")
+// document.write("Percentage: "+ percentage +"%")
+
+// TASK 09
+// var usDollar = 10
+// var eachDollar = 104.80
+// var uaeRiyal = 25
+// var eachRiyal = 28
+// document.write("Currency in PKR".bold() +"<br>"+"<br>")
+// document.write("Total Currency in PKR: " + ((usDollar*eachDollar) + (uaeRiyal*eachRiyal)))
+
+// TASK 10
+// var num = 15
+// var num = ((num + 5) * 10) / 2
+// alert(num)
+
+// TASK 11
+// var currentYear = 2020
+// var birthYear = 1998
+// var age = currentYear - birthYear
+
+// document.write("Age Calculator".bold() +"<br>"+"<br>")
+// document.write("Current Year: "+ currentYear +"<br>")
+// document.write("Birth Year: "+ birthYear +"<br>")
+// document.write("Your Age is: "+ age)
+
+// TASK 12
+// var radius = 20
+// var pi = 3.142
+// var circumference = 2 * pi * radius
+// var area = (pi * Math.pow(radius, 2))
+// document.write("The Geometizer".bold() +"<br>"+"<br>")
+// document.write("Radius of Circle: "+ radius +"<br>")
+// document.write("The circumference is: "+ circumference +"<br>")
+// document.write("The area is: "+ area)
+
+// Task 13
+// var favSnack = "Cookies" 
+// var currentAge = 20
+// var maxAge = 55
+// var ammount = 4
+// document.write("The Lifetime Supply Calculator".bold() +"<br>"+"<br>")
+// document.write("Favourite snack: "+ favSnack +"<br>")
+// document.write("Current Age: "+ currentAge +"<br>")
+// document.write("Estimated Maximum Age: "+ maxAge +"<br>")
+// document.write("Ammount of snacks per day: "+ ammount +"<br>")
+// document.write("You will need "+ ((maxAge-currentAge)*ammount) +"\t"+ favSnack + 
+//                 " to last you until the ripe old age of "+ maxAge
+// )
+
+
+// CHAPTER #6 - 9
+
+
+// Task 01
+// var a = 10
+// document.write("Rsult"+"<br>")
+// document.write("The Value of a is: "+ a +"<br>")
+// document.write("-------------------------------"+"<br>")
+// document.write("The value of ++a is: "+ ++a +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+// document.write("The value of a++ is: "+ a++ +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+// document.write("The value of --a is: "+ --a +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+// document.write("The value of a-- is: "+ a-- +"<br>")
+// document.write("Now the value of a is: "+ a +"<br>"+"<br>")
+
+// Task 02
+// var a = 2, b = 1
+// document.write("a is: "+ a +"<br>")
+// document.write("b is: "+ b +"<br>")
+
+// var result = --a - --b + ++b + b--
+// document.write("Result is: "+ result)
+
+// Task 03
+// var name = prompt("Enter name")
+// alert("Hello "+ name)
+
+// Task 04
+// var num = prompt("Enter a number table would be displayed")
+
+// document.write("Table of  ".bold() + num +"<br>"+"<br>")
+// var num2 = 1
+
+// document.write(num +" * "+ num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+// document.write(num +" * "+ ++num2 +" = " + num*num2 +"<br>")
+
+// Task 05
+
+
+
